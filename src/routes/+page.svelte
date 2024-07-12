7288133d8f80b341be08e1511f49c36d81d6b41a
sadministrator <marco@melproject.org>
2024-07-11 18:09:55 -0600

    loading in main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,105 +10,118 @@
 	let myDebts: Debts = {};
 	let nodes: string[] = [];
 	let edges: [string, string][] = [];
+	let isLoading = true;
 
 	onMount(async () => {
-		const { myFingerprint, relays, adjacencies, neighbors } = await fetchRelayGraphInfo();
+		try {
+			const { myFingerprint, relays, adjacencies, neighbors } = await fetchRelayGraphInfo();
 
-		myId = myFingerprint;
-		nodes = relays;
-		edges = adjacencies;
-		myNeighbors = neighbors;
-		myDebts = await fetchDebts();
+			myId = myFingerprint;
+			nodes = relays;
+			edges = adjacencies;
+			myNeighbors = neighbors;
+			myDebts = await fetchDebts();
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		} finally {
+			isLoading = false;
+		}
 	});
 </script>
 
-<div class="container-fluid">
-	<div class="row">
-		<section class="col-md-2">
-			<h2>Debts</h2>
-			<ul class="list-unstyled">
-				{#each Object.entries(myDebts) as [neighbor, amount]}
-					<li class="mb-3">
+{#if isLoading}
+	<p>Loading...</p>
+{:else}
+	<div class="container-fluid">
+		<div class="row">
+			{#if Object.keys(myDebts).length > 0}
+				<section class="col-md-2">
+					<h2>Debts</h2>
+					<ul class="list-unstyled">
+						{#each Object.entries(myDebts) as [neighbor, amount]}
+							<li class="mb-3">
+								<div class="card">
+									<div class="card-body">
+										<h5 class="card-title">{neighbor}</h5>
+										<p class="card-text">{amount} micromel</p>
+									</div>
+								</div>
+							</li>
+						{/each}
+					</ul>
+				</section>
+			{/if}
+
+			{#if edges.length > 0 && nodes.length > 0}
+				<section class="col-md-6">
+					<h2>Relay Graph</h2>
+					<div class="card">
+						<div class="card-body">
+							<VisGraph {nodes} {edges} />
+						</div>
+					</div>
+				</section>
+			{/if}
+
+			{#if myId}
+				<section class="col-md-4">
+					<h2>Bandwidth</h2>
+					<div class="mb-3">
 						<div class="card">
 							<div class="card-body">
-								<h5 class="card-title">{neighbor}</h5>
-								<p class="card-text">{amount} micromel</p>
+								<TimeSeries
+									node={myId}
+									direction={Direction.Down}
+									title="Bandwidth Down"
+									unit="bytes/second"
+								/>
 							</div>
 						</div>
-					</li>
-				{/each}
-			</ul>
-		</section>
-
-		<section class="col-md-6">
-			<h2>Relay Graph</h2>
-			<div class="card">
-				<div class="card-body">
-					{#if edges && edges.length > 0}
-						<VisGraph {nodes} {edges} />
-					{:else}
-						<p class="text-center">Loading...</p>
-					{/if}
-				</div>
-			</div>
-		</section>
-
-		<section class="col-md-4">
-			<h2>Bandwidth</h2>
-			<div class="mb-3">
-				<div class="card">
-					<div class="card-body">
-						<TimeSeries
-							node={myId}
-							direction={Direction.Down}
-							title="Bandwith Down"
-							unit="bytes/second"
-						/>
 					</div>
-				</div>
-			</div>
-			<div class="mb-3">
-				<div class="card">
-					<div class="card-body">
-						<TimeSeries
-							node={myId}
-							direction={Direction.Up}
-							title="Bandwidth Up"
-							unit="bytes/second"
-						/>
-					</div>
-				</div>
-			</div>
-
-			{#each myNeighbors as neighbor}
-				<div class="mb-3">
-					<div class="card">
-						<div class="card-body">
-							<TimeSeries
-								node={neighbor}
-								direction={Direction.Down}
-								title="Bandwith Down"
-								unit="bytes/second"
-							/>
+					<div class="mb-3">
+						<div class="card">
+							<div class="card-body">
+								<TimeSeries
+									node={myId}
+									direction={Direction.Up}
+									title="Bandwidth Up"
+									unit="bytes/second"
+								/>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="mb-3">
-					<div class="card">
-						<div class="card-body">
-							<TimeSeries
-								node={neighbor}
-								direction={Direction.Up}
-								title="Bandwidth Up"
-								unit="bytes/second"
-							/>
+
+					{#each myNeighbors as neighbor}
+						<div class="mb-3">
+							<div class="card">
+								<div class="card-body">
+									<TimeSeries
+										node={neighbor}
+										direction={Direction.Down}
+										title="Bandwidth Down"
+										unit="bytes/second"
+									/>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			{/each}
-		</section>
+						<div class="mb-3">
+							<div class="card">
+								<div class="card-body">
+									<TimeSeries
+										node={neighbor}
+										direction={Direction.Up}
+										title="Bandwidth Up"
+										unit="bytes/second"
+									/>
+								</div>
+							</div>
+						</div>
+					{/each}
+				</section>
+			{/if}
+		</div>
 	</div>
-</div>
+{/if}
 
 <style>
 	.card {
